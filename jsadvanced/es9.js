36a93211e9977c64ae901d65fc7437f18219155c
Junela <juneladedja@gmail.com>
2023-11-23 18:36:16 +0100

    es3 template strings

---
+++ jsadvanced/es9.js
@@ -0,0 +1,9 @@
+// Crea una template string che genera un messaggio 
+// di saluto con un nome e un messaggio personalizzato.
+
+
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `John Welcome to our website!`
+
+console.log(greeting);
