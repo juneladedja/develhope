b1ff056d38981634a87dce3af1abf4d4048e7586
Junela <juneladedja@gmail.com>
2023-11-23 18:33:56 +0100

    es2 template strings

---
+++ jsadvanced/es8.js
@@ -0,0 +1,7 @@
+// Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+const string = `Utilizza una template 
+string per creare una stringa su più 
+righe che includa interruzioni di riga.`
+
+console.log(string);
