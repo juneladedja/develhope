5de176be9ee63c3cfc5405debfab7b34ece8c471
Junela <juneladedja@gmail.com>
2023-11-26 08:32:39 +0100

    es2 interval

---
+++ jsadvanced/es20.js
@@ -0,0 +1,16 @@
+// Crea una funzione chiamata "contoAllaRovescia" 
+// che diminuirà il conto alla rovescia di 1 unità e 
+// visualizzerà il tempo rimanente in console.
+// Utilizza setInterval per chiamare la funzione contoAllaRovescia ogni secondo.
+// Visualizza il messaggio "Conto alla rovescia in corso..." all'inizio.
+// Quando il conto alla rovescia raggiunge 0, 
+// visualizza un messaggio che indica che il conto alla rovescia è terminato.
+
+let tempoRimanente = 10;
+
+function contoAllaRovescia() {
+    tempoRimanente--;
+    console.log(`conto alla rovescia in corso... ${tempoRimanente}`);
+}
+
+const id = setInterval(contoAllaRovescia, 1000);
