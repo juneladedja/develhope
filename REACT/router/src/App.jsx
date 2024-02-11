dc4014095429163cb297437bfaf9e4e6d36fe0eb
Junela <juneladedja@gmail.com>
2024-02-11 17:08:49 +0100

    router 1-5

---
+++ REACT/router/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import { Welcome } from './component/Welcome';
+import { Counter } from './component/Counter';
+import { ShowGitHubUser } from './component/ShowGitHubUser';
+import { NotFound } from './component/NotFound';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Welcome name="June" age="26" />} />
+        <Route path='/contatore' element={<Counter />} />
+        <Route path='/utenti/:username' element={<ShowGitHubUser />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      
+      <Link to={"/contatore"}> Counter </Link>
+      <Link to={"/"}> Welcome </Link>
+      <Link to={"/utenti/juneladedja"}> ShowGitHubUser </Link>
+      
+    </>
+  );
+}
+
+export default App;
