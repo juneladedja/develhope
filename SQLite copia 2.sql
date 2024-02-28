b148e08be162c707b38023e1f1512d3dabccd9e4
Junela Dedja <148102178+juneladedja@users.noreply.github.com>
2024-02-28 14:29:17 +0100

    Add files via upload

---
+++ SQLite copia 2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM demo;
+
+INSERT INTO Libri (book_id, titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count)
+VALUES (1, 'Viaggio al centro', 'Jules Verne', 'Avventura', 1864, '1234567890123', 12.99, 4.5, 10);
+
+INSERT INTO Libri (book_id, titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count)
+VALUES (2, 'Guerra e pace', 'Lev Tolstoj', 'Storico', 1869, '1234567890124', 14.99, 4.8, 5);
+
+INSERT INTO Libri (book_id, titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count)
+VALUES (3, 'I sussurri del vento', 'Amelia Blackburn', 'Romantico', 1982, '1234567890125', 9.99, 4.2, 20);
+
+INSERT INTO Libri (book_id, titolo, autore, genere, pubblicato_anno, isbn, prezzo, valutazione, stock_count)
+VALUES (4, 'L''Odissea Galattica', 'Orion Starfield', 'Fantascienza', 2005, '1234567890126', 19.99, 4.9, 15);
