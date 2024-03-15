4e893f89cdfe24c70abe24ba179050415f2d2343
Junela <juneladedja@gmail.com>
2024-03-15 15:40:16 +0100

    callbacks node

--- NODE/es8.js/script.mjs
+++ NODE/es8.js/script.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs'
 
 const contenuto = 'il mio testo'
 
-fs.writeFile('FileProva.txt' , contenuto , (err) => {
+fs.writeFile('text.txt' , contenuto , (err) => {
     if(err){
         console.log('Attenzione errore');
         return
